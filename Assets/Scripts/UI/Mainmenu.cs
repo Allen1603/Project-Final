74dd5895b835bb15da24d29d77c9342bcd646a6e
RuzzNel <163853877+RossNel15@users.noreply.github.com>
2025-11-27 01:30:19 +0800

    Tutorial

--- Assets/Scripts/UI/Mainmenu.cs
+++ Assets/Scripts/UI/Mainmenu.cs
@@ -6,11 +6,19 @@ public class Mainmenu : MonoBehaviour
     public void Play()
     {
         SceneManager.LoadSceneAsync(1);
+        Time.timeScale = 1f;
     }
 
     public void Tutorial()
     {
         SceneManager.LoadSceneAsync(3);
+        Time.timeScale = 1f;
+    }
+
+    public void Exit()
+    {
+        SceneManager.LoadSceneAsync(0);
+        Time.timeScale = 1f;
     }
 
     public void Quit()
