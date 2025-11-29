87d0a71607e992d4392c7ee4d05bd4a5a7edc265
NelRoss <jrgnavalle@astean.biscast.edu.ph>
2025-11-30 04:56:43 +0800

    Almanyak

--- Assets/Scripts/AlmanacManDaa/FrogAndInsectSelection.cs
+++ Assets/Scripts/AlmanacManDaa/FrogAndInsectSelection.cs
@@ -1,22 +1,33 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class FrogAndInsectSelection : MonoBehaviour
 {
-    public GameObject[] pics;       // drag pic1, pic2, pic3, pic4 here
-    public GameObject[] descriptions; // drag Idescrip1, Idescrip2, Idescrip3, Idescrip4 here
+    public GameObject[] pics;          // assign pic1, pic2, pic3, pic4
+    public GameObject[] descriptions;  // assign Idescrip1, Idescrip2, Idescrip3, Idescrip4
+    public Button[] buttons;           // assign buttons corresponding to each card
+
+    public Color selectedColor = new Color(0f, 0f, 0f, 0.5f); // dark & semi-transparent
+    public Color normalColor = new Color(1f, 1f, 1f, 1f);     // fully opaque white
 
     void Start()
     {
         ShowCard(0); // show first card by default
     }
 
-    // Call this from buttons: pass 0 for first card, 1 for second, etc.
     public void ShowCard(int index)
     {
         for (int i = 0; i < pics.Length; i++)
         {
+            // Activate selected card and description
             pics[i].SetActive(i == index);
             descriptions[i].SetActive(i == index);
+
+            // Change button color
+            if (buttons != null && buttons.Length > i)
+            {
+                buttons[i].GetComponent<Image>().color = (i == index) ? selectedColor : normalColor;
+            }
         }
     }
 }
