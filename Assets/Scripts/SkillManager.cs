cc8d041166bdc533b993171d0aeb22fb4d3a3e6f
Allen1603 <sanjoseallen16@gmail.com>
2025-12-01 00:30:06 +0800

    try fix

--- Assets/Scripts/SkillManager.cs
+++ Assets/Scripts/SkillManager.cs
@@ -30,6 +30,7 @@ public class SkillManager : MonoBehaviour
     [Header("Clone Skill")]
     public GameObject clonePrefab;  // DRAG your CloneSkill prefab here
     public Transform clonePosition;
+    private FrogData selectedFrog;
 
     [Header("Skill Button")]
     public Button stunButton;
@@ -48,6 +49,11 @@ public class SkillManager : MonoBehaviour
     {
         player = PlayerController.instance;
         UpdateSkillButtons(0, player.MaxBar);
+
+        selectedFrog = FrogSelectionManager.instance.selectedFrog;
+
+        // Replace the default clonePrefab with the selected frog's clonePrefab
+        clonePrefab = selectedFrog.clonePrefab;
     }
 
     #region Stun Skill
