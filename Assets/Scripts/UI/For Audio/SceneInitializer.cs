a2fe23586ad139f2ba6bc8a7d85bb9859ebdee0e
RuzzNel <163853877+RossNel15@users.noreply.github.com>
2025-12-01 10:31:23 +0800

    fixed
    - pause button and pause panel
    - fix audio for picking scene
    - dae ko pa natatry si loading itong palaka na dae nag lalagpas sa loading bar

--- Assets/Scripts/UI/For Audio/SceneInitializer.cs
+++ Assets/Scripts/UI/For Audio/SceneInitializer.cs
@@ -7,15 +7,10 @@ public class SceneInitializer : MonoBehaviour
 {
     void Start()
     {
-        if (AudioManager.Instance == null) return;
-
-        string sceneName = SceneManager.GetActiveScene().name;
-
-        AudioManager.Instance.StopBGM(); // stop previous BGM
-
-        if (sceneName == "Almanac")
-            AudioManager.Instance.PlayBGM("AlmanacBGM");
-        else if (sceneName == "MainMenu")
-            AudioManager.Instance.PlayBGM("MainMenu");
+        if (AudioManager.Instance != null)
+        {
+            AudioManager.Instance.StopBGM();
+            AudioManager.Instance.PlayBGM("PickingAudio");
+        }
     }
 }
